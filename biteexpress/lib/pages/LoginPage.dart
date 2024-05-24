d218988d63df288aa74984c9d23c727e50332a9d
Q00R <ripbaelol@gmail.com>
2024-05-24 12:55:32 +0300

    bad authentication round 1

---
+++ biteexpress/lib/pages/LoginPage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import './home.dart';
+
+
+
+class LoginSignUpPage extends StatelessWidget {
+  const LoginSignUpPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
