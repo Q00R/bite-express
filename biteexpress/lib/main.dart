e3d14bb345ef51439c64739ed8ed8ee0f5d24006
Q00R <ripbaelol@gmail.com>
2024-05-24 09:41:00 +0300

    how to make the project unrunnable

--- biteexpress/lib/main.dart
+++ biteexpress/lib/main.dart
@@ -7,7 +7,10 @@ import './widgets/bottomNavigation.dart';
 import './providers/productsProvider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-void main() {
+void main() async{
+WidgetsFlutterBinding.ensureInitialized();
+await Firebase.initializeApp();
+
   runApp(
     MultiProvider(
       providers: [
