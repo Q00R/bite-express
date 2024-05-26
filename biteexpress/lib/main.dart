b3485672f890e257a0bc119106fd94538719966a
Q00R <ripbaelol@gmail.com>
2024-05-26 05:21:30 +0300

    added debugging and crashlytics

--- biteexpress/lib/main.dart
+++ biteexpress/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:biteexpress/pages/MorePage.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import './pages/home.dart';
@@ -11,8 +12,18 @@ import './pages/cartPage.dart';
 import './pages/Signup.dart';
 import './pages/SignIn.dart';
 import './providers/authenticationProvider.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+
+
+
+void main()async {
+   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+  // Pass all uncaught "fatal" errors from the framework to Crashlytics
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+  
   runApp(
     MultiProvider(
       providers: [
